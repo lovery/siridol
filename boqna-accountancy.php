8daa10424288290fb1522f75f57f6f9bb36232d7
Jivka Gancheva <ji.gancheva@gmail.com>
2010-09-11 12:20:23 +0300

    Total chenging the file!!!

--- boqna-accountancy.php
+++ boqna-accountancy.php
@@ -1,36 +1,44 @@
 <html>
 <head>
-<meta http-equiv='accounting' charset='UTF-8'/>
+<meta http-equiv="accounting" charset="UTF-8"/>
 <link rel="stylesheet" type="text/css" href="boqna-accountancy.css">
 <title>
 Счетоводство на комплекса
 </title>
 </head>
-<body>
-<table border="0" cellspacing="0" cellpadding="0" bgcolor="#CCCCCC">
-<tr>
-<td>
+<body bgcolor="#EEEEEE">
 <?php
-$connect=mysql_connect('localhost', 'root', 'happypass');
+
+function print_html_num_td($num) {
+    if ($num<0) {
+        printf( "<td align='right' bgcolor='red'><nobr>$num</nobr></td>\n");
+    }
+    else {
+        printf( "<td align='right'><nobr>$num</nobr></td>\n");
+    }
+}
+
+$connect=mysql_connect("localhost", "root", "happypass");
 if (!$connect) {
-        die ('There is a problem with connect to the MySQL Server<br/>');
+        die ("There is a problem with connect to the MySQL Server<br/>");
 }   
 
-$result=mysql_select_db('test');
+$result=mysql_select_db("test");
 if (!$result) {
-    echo 'There is a problem with loading the database<br/>';
+    printf( "There is a problem with loading the database<br/>");
 }
 
-$sql=' SELECT date_format(Month, "%Y-%m"), date_format(Date, "%Y.%m.%d"), Rubbish, Greenarea, homemanager, cleanstreets, fund, total, explanation from boqna';
+$sql=" SELECT date_format(Month, '%Y.%m') as month, date_format(Date, '%Y.%m.%d') as data, Rubbish, Greenarea, homemanager, cleanstreets, fund, total, explanation from boqna";
 $sql_result=mysql_query($sql);
 if (!$sql_result) {
-    echo 'There is a problem with selectin from database<br/>';
+    printf( "There is a problem with selectin from database<br/>");
     $error=mysql_error();
-    echo 'The error is :'.$error.'<br/';
+    printf( "The error is :".$error."<br/>");
 }
 
-echo '<table class="tableFormating" border="0" cellspacing="1" cellpadding="1">';
-echo '<tr  bgcolor="white"><th>За месец</th>
+printf("<table class='tableBorder'>\n");
+printf("<tr>
+    <th>За месец</th>
     <th>Дата</th>
     <th>Смет</th>
     <th>Зелени площи</th>
@@ -39,48 +47,41 @@ echo '<tr  bgcolor="white"><th>За месец</th>
     <th>Фонд</th>
     <th>Общо</th>
     <th>Пояснение</th>
-    </tr>';
+    </tr>");
 
-while ($array=mysql_fetch_array($sql_result, MYSQL_NUM)) {
-    echo '<tr bgcolor="white">';
-    for ($count=0; $count<9; $count++) {
-        if ($count!=8 and $count!=0) {
-            if ($array[$count]<0) {
-                echo '<td align="right" bgcolor="red"><nobr>'.$array[$count].'</nobr></td>';
-            }
-            else {
-                echo '<td align="right"><nobr>'.$array[$count].'</nobr></td>';
-            }
-            if ($count!=0 and $count!=1 and $count!=8) {
-                $countin[$count-2]+=$array[$count];
-            }
-            
-            }
-            else {
-                echo '<td><nobr>'.$array[$count].'</nobr></td>';
-            }
-    }
-    echo '</tr>';
+while ($array=mysql_fetch_array($sql_result, MYSQL_ASSOC)) {
+    printf("<tr>\n");
+    printf("<td>$array[month]</td>\n");
+    printf("<td>$array[data]</td>\n");
+    print_html_num_td($array[Rubbish]);
+    print_html_num_td($array[Greenarea]);
+    print_html_num_td($array[homemanager]);
+    print_html_num_td($array[cleanstreets]);
+    print_html_num_td($array[fund]);
+    print_html_num_td($array[total]);
+    printf("<td><nobr>$array[explanation]</nobr></td>\n");
+    printf("</tr>\n");
 }
-echo '<tr align="right" bgcolor="white"> <th>&nbsp;</th>
-    <th>Общо:</th>
-    <th>'.$countin[0].'</th>
-    <th>'.$countin[1].'</th>
-    <th>'.$countin[2].'</th>
-    <th>'.$countin[3].'</th>
-    <th>'.$countin[4].'</th>
-    <th>'.$countin[5].'</th>
+
+$total_sql=mysql_query("select sum(Rubbish) as trub, sum(Greenarea) as tgreen, sum(homemanager) as thome, sum(cleanstreets) as tclean, sum(fund) as tfund, sum(total) as total from boqna");
+$total=mysql_fetch_array($total_sql, MYSQL_ASSOC);
+printf("<tr align='right'>
     <th>&nbsp;</th>
-    </tr>';
-echo '</table>';
+    <th>Общо:</th>
+    <th>$total[trub]</th>
+    <th>$total[tgreen]</th>
+    <th>$total[thome]</th>
+    <th>$total[tclean]</th>
+    <th>$total[tfund]</th>
+    <th>$total[total]</th>
+    <th>&nbsp;</th>
+    </tr>\n");
+printf("</table>\n");
 
 if (!mysql_close($connect)) {
-        die ('There is a problem with closing the connection<br/>');
+        die ("There is a problem with closing the connection<br/>");
 }   
 ?>
-</td>
-</tr>
-</table>
 </body>
 </html>
 
