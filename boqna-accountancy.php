e014932d95a649cd319cec977feebc9036452ba2
Jivka Gancheva <ji.gancheva@gmail.com>
2010-09-14 11:00:01 +0300

    Done some of the work-task.

--- boqna-accountancy.php
+++ boqna-accountancy.php
@@ -21,108 +21,129 @@ function print_html_num_td($num) {
 
 $connect=mysql_connect("localhost", "root", "happypass");
 if (!$connect) {
-        die ("There is a problem with connect to the MySQL Server<br/>");
+    die("mysql_connect: ".mysql_error()."<br/>");
 }   
 
 $result=mysql_select_db("test");
 if (!$result) {
-    printf( "There is a problem with loading the database<br/>");
+    die("mysql_select_db :".mysql_error()."<br/>");
 }
 
-printf("<table class='tableBorder'>\n");
-printf("<tr>
-    <th><nobr>За месец<a href=?sort=1&type=asc><img src='down_arrow.png'/></a><a href=?sort=1&type=desc><img src='up_arrow.png'/></a></nobr></th>
-    <th><nobr>Дата<a href=?sort=2&type=asc><img src='down_arrow.png'/></a><a href=?sort=2&type=desc><img src='up_arrow.png'/></a></nobr></th>
-    <th><nobr>Смет<a href=?sort=3&type=asc><img src='down_arrow.png'/></a><a href=?sort=3&type=desc><img src='up_arrow.png'/></a><nobr></th>
-    <th>Зелени <nobr>площи<a href=?sort=4&type=asc><img src='down_arrow.png'/></a><a href=?sort=4&type=desc><img src='up_arrow.png'/></a></nobr></th>
-    <th><nobr>Домоупр.<a href=?sort=5&type=asc><img src='down_arrow.png'/></a><a href=?sort=5&type=desc><img src='up_arrow.png'/></a></nobr></th>
-    <th>Почист. <nobr>улици<a href=?sort=6&type=asc><img src='down_arrow.png'/></a><a href=?sort=6&type=desc><img src='up_arrow.png'/></a></nobr></th>
-    <th><nobr>Фонд<a href=?sort=7&type=asc><img src='down_arrow.png'/></a><a href=?sort=7&type=desc><img src='up_arrow.png'/></a></nobr></th>
-    <th><nobr>Общо<a href=?sort=8&type=asc><img src='down_arrow.png'/></a><a href=?sort=8&type=desc><img src='up_arrow.png'/></a></nobr></th>
-    <th><nobr>Пояснение<a href=?sort=9&type=asc><img src='down_arrow.png'/></a><a href=?sort=9&type=desc><img src='up_arrow.png'/></a></nobr></th>
-</tr>");
+for ($i=0; $i<9; $i++) {
+    $name_img[$i][0]="down_arrow_origin.png";
+    $name_img[$i][1]="up_arrow_origin.png";
+}
 
 switch ($_GET["sort"]) {
 case 1: 
     if ($_GET["type"]=="asc") {
         $sqlpl=" order by Month, Date";
+        $name_img[0][0]="down_arrow.png";
     }
     else {
         $sqlpl=" order by Month DESC, Date";
+        $name_img[0][1]="up_arrow.png";
     }
     break;
 case 2:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by Date";
+        $sqlpl=" order by Date, Month";
+        $name_img[1][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by Date DESC";
+        $sqlpl=" order by Date DESC, Month";
+        $name_img[1][1]="up_arrow.png";
     }
     break;
 case 3:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by Rubbish, Date";
+        $sqlpl=" order by Rubbish, Month, Date";
+        $name_img[2][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by Rubbish DESC, Date";
+        $sqlpl=" order by Rubbish DESC, Month, Date";
+        $name_img[2][1]="up_arrow.png";
     }
     break;
 case 4:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by Greenarea, Date";
+        $sqlpl=" order by Greenarea, Month, Date";
+        $name_img[3][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by Greenarea DESC, Date";
+        $sqlpl=" order by Greenarea DESC, Month, Date";
+        $name_img[3][1]="up_arrow.png";
     }
     break;
 case 5:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by homemanager, Date";
+        $sqlpl=" order by homemanager, Month, Date";
+        $name_img[4][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by homemanager DESC, Date";
+        $sqlpl=" order by homemanager DESC, Month, Date";
+        $name_img[4][1]="up_arrow.png";
     }
     break;
 case 6:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by cleanstreets, Date";
+        $sqlpl=" order by cleanstreets, Month, Date";
+        $name_img[5][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by cleanstreets DESC, Date";
+        $sqlpl=" order by cleanstreets DESC, Month, Date";
+        $name_img[5][1]="up_arrow.png";
     }
     break;
 case 7:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by fund, Date";
+        $sqlpl=" order by fund, Month, Date";
+        $name_img[6][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by fund DESC, Date";
+        $sqlpl=" order by fund DESC, Month, Date";
+        $name_img[6][1]="up_arrow.png";
     }
     break;
 case 8:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by Rubbish+Greenarea+homemanager+cleanstreets+fund, Date";
+        $sqlpl=" order by Rubbish+Greenarea+homemanager+cleanstreets+fund, Month, Date";
+        $name_img[7][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by Rubbish+Greenarea+homemanager+cleanstreets+fund DESC, Date";
+        $sqlpl=" order by Rubbish+Greenarea+homemanager+cleanstreets+fund DESC, Month, Date";
+        $name_img[7][1]="up_arrow.png";
     }
     break;
 case 9:
     if ($_GET["type"]=="asc") {
-        $sqlpl=" order by explanation, Date";
+        $sqlpl=" order by explanation, Month, Date";
+        $name_img[8][0]="down_arrow.png";
     }
     else {
-        $sqlpl=" order by explanation DESC, Date";
+        $sqlpl=" order by explanation DESC, Month, Date";
+        $name_img[8][1]="up_arrow.png";
     }
     break;
 };
 
+printf("<table class='tableBorder'>\n");
+printf("<tr>
+    <th><nobr>За месец<a href=?sort=1&type=asc><img src=".$name_img[0][0]." /></a><a href=?sort=1&type=desc><img src=".$name_img[0][1]." /></a></nobr></th>
+    <th><nobr>Дата<a href=?sort=2&type=asc><img src=".$name_img[1][0]." /></a><a href=?sort=2&type=desc><img src=".$name_img[1][1]." /></a></nobr></th>
+    <th><nobr>Смет<a href=?sort=3&type=asc><img src=".$name_img[2][0]." /></a><a href=?sort=3&type=desc><img src=".$name_img[2][1]." /></a><nobr></th>
+    <th>Зелени <nobr>площи<a href=?sort=4&type=asc><img src=".$name_img[3][0]." /></a><a href=?sort=4&type=desc><img src=".$name_img[3][1]." /></a></nobr></th>
+    <th><nobr>Домоупр.<a href=?sort=5&type=asc><img src=".$name_img[4][0]." /></a><a href=?sort=5&type=desc><img src=".$name_img[4][1]." /></a></nobr></th>
+    <th>Почист. <nobr>улици<a href=?sort=6&type=asc><img src=".$name_img[5][0]." /></a><a href=?sort=6&type=desc><img src=".$name_img[5][1]." /></a></nobr></th>
+    <th><nobr>Фонд<a href=?sort=7&type=asc><img src=".$name_img[6][0]." /></a><a href=?sort=7&type=desc><img src=".$name_img[6][1]." /></a></nobr></th>
+    <th><nobr>Общо<a href=?sort=8&type=asc><img src=".$name_img[7][0]." /></a><a href=?sort=8&type=desc><img src=".$name_img[7][1]." /></a></nobr></th>
+    <th><nobr>Пояснение<a href=?sort=9&type=asc><img src=".$name_img[8][0]." /></a><a href=?sort=9&type=desc><img src=".$name_img[8][1]." /></a></nobr></th>
+</tr>");
+
 $sql="SELECT date_format(Month, '%Y.%m') as month, date_format(Date, '%Y.%m.%d') as data, Rubbish, Greenarea, homemanager, cleanstreets, fund, Rubbish+Greenarea+homemanager+cleanstreets+fund as total, explanation from boqna".$sqlpl;
 $sql_result=mysql_query($sql);
 if (!$sql_result) {
-    printf( "There is a problem with selecting from database<br/>");
-    $error=mysql_error();
-    die( "The error is :".$error."<br/>");
+    die( "Mysql_query :".mysql_error()."<br/>");
 }
 
 while ($array=mysql_fetch_array($sql_result, MYSQL_ASSOC)) {
@@ -141,9 +162,7 @@ while ($array=mysql_fetch_array($sql_result, MYSQL_ASSOC)) {
 
 $total_sql=mysql_query("select trub, tgreen, thome, tclean, tfund, trub+tgreen+thome+tclean+tfund as total from (select sum(Rubbish) as trub, sum(Greenarea) as tgreen, sum(homemanager) as thome, sum(cleanstreets) as tclean, sum(fund) as tfund from boqna) as tab");
 if (!$total_sql) {
-    printf( "There is a problem with selecting from database<br/>");
-    $error=mysql_error();
-    die( "The error is :".$error."<br/>");
+    die( "Mysql_query :".mysql_error()."<br/>");
 }
 $total=mysql_fetch_array($total_sql, MYSQL_ASSOC);
 printf("<tr align='right'>
