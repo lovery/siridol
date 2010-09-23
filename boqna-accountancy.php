4829dbf625c155767361f8d7a351e1ba91152b77
Jivka Gancheva <ji.gancheva@gmail.com>
2010-09-23 21:32:27 +0300

    Add the sum row after month.

--- boqna-accountancy.php
+++ boqna-accountancy.php
@@ -1,11 +1,20 @@
 <html>
 <head>
-<meta http-equiv="accounting" charset="UTF-8"/>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <link rel="stylesheet" type="text/css" href="boqna-accountancy.css">
 <title>
 Счетоводство на комплекса
 </title>
+<script type="text/javascript">
+function add_row() {
+	document.getElementById("insert").style.display="block";
+}
+function remove_row() {
+	document.getElementById("insert").style.display="none";
+}
+</script>
 </head>
+
 <body bgcolor="#EEEEEE">
 <?php
 
@@ -131,28 +140,28 @@ case 9:
 
 printf("<table class='tableBorder'>\n");
 printf("<tr>
-	<th><nobr>За месец<a href=?sort=1&type=asc><img
+	<th class='width_td'><nobr>За месец<a href=?sort=1&type=asc><img
 	src=".$name_img[0][0]." /></a><a href=?sort=1&type=desc><img
 	src=".$name_img[0][1]." /></a></nobr></th>
-	<th><nobr>Дата<a href=?sort=2&type=asc><img
+	<th class='width_td'><nobr>Дата<a href=?sort=2&type=asc><img
 	src=".$name_img[1][0]." /></a><a href=?sort=2&type=desc><img
 	src=".$name_img[1][1]." /></a></nobr></th>
-	<th><nobr>Смет<a href=?sort=3&type=asc><img
+	<th class='width_td'><nobr>Смет<a href=?sort=3&type=asc><img
 	src=".$name_img[2][0]." /></a><a href=?sort=3&type=desc><img
 	src=".$name_img[2][1]." /></a><nobr></th>
-	<th>Зелени <nobr>площи<a href=?sort=4&type=asc><img
+	<th class='width_td'>Зелени <nobr>площи<a href=?sort=4&type=asc><img
 	src=".$name_img[3][0]." /></a><a href=?sort=4&type=desc><img
 	src=".$name_img[3][1]." /></a></nobr></th>
-	<th><nobr>Домоупр.<a href=?sort=5&type=asc><img
+	<th class='width_td'><nobr>Домоупр.<a href=?sort=5&type=asc><img
 	src=".$name_img[4][0]." /></a><a href=?sort=5&type=desc><img
 	src=".$name_img[4][1]." /></a></nobr></th>
-	<th>Почист. <nobr>улици<a href=?sort=6&type=asc><img
+	<th class='width_td'>Почист. <nobr>улици<a href=?sort=6&type=asc><img
 	src=".$name_img[5][0]." /></a><a href=?sort=6&type=desc><img
 	src=".$name_img[5][1]." /></a></nobr></th>
-	<th><nobr>Фонд<a href=?sort=7&type=asc><img
+	<th class='width_td'><nobr>Фонд<a href=?sort=7&type=asc><img
 	src=".$name_img[6][0]." /></a><a href=?sort=7&type=desc><img
 	src=".$name_img[6][1]." /></a></nobr></th>
-	<th><nobr>Общо<a href=?sort=8&type=asc><img
+	<th class='width_td'><nobr>Общо<a href=?sort=8&type=asc><img
 	src=".$name_img[7][0]." /></a><a href=?sort=8&type=desc><img
 	src=".$name_img[7][1]." /></a></nobr></th>
 	<th><nobr>Пояснение<a href=?sort=9&type=asc><img
@@ -168,18 +177,39 @@ if (!$sql_result) {
 	die("Mysql_query :".mysql_error()."<br/>");
 }
 
+$is_date='ne e data';
 while ($array=mysql_fetch_array($sql_result, MYSQL_ASSOC)) {
+	if ($is_date!=$array[month] & $_GET["sort"]=="1" & $is_date!='ne e data') {
+		printf("<tr>\n");
+		printf("<td class='nonegative' colspan=2>Общо за месец ".$is_date."</td>\n");
+		print_html_num_td($is_rub);
+		print_html_num_td($is_green);
+		print_html_num_td($is_home);
+		print_html_num_td($is_clean);
+		print_html_num_td($is_fund);
+		print_html_num_td($is_total);
+		printf("<td>&nbsp;</td>\n");
+		printf("</tr>\n");
+		$is_rub=$is_green=$is_home=$is_clean=$is_fund=$is_total=0;
+	}
 	printf("<tr>\n");
 	printf("<td>$array[month]</td>\n");
 	printf("<td>$array[data]</td>\n");
 	print_html_num_td($array[Rubbish]);
+	$is_rub+=$array[Rubbish];
 	print_html_num_td($array[Greenarea]);
+	$is_green+=$array[Greenarea];
 	print_html_num_td($array[homemanager]);
+	$is_home+=$array[homemanager];
 	print_html_num_td($array[cleanstreets]);
+	$is_clean+=$array[cleanstreets];
 	print_html_num_td($array[fund]);
+	$is_fund+=$array[fund];
 	print_html_num_td($array[total]);
+	$is_total+=$array[total];
 	printf("<td><nobr>$array[explanation]</nobr></td>\n");
 	printf("</tr>\n");
+	$is_date=$array[month];
 }
 
 $total_sql=mysql_query("select trub, tgreen, thome, tclean, tfund,
@@ -189,6 +219,20 @@ $total_sql=mysql_query("select trub, tgreen, thome, tclean, tfund,
 if (!$total_sql) {
 	die( "Mysql_query :".mysql_error()."<br/>");
 }
+
+printf("<tr class=tableBorderAdd>
+	<td colspan=9><table class='add_row' id='insert'><tr><form>
+	<td><input type='text' size=7 name='month'/></td>
+	<td><input type='text' size=10 name='data_come'/></td>
+	<td><input type='text' size=8 name='rub'/></td>
+	<td><input type='text' size=8 name='area'/></td>
+	<td><input type='text' size=8 name='hman'/></td>
+	<td><input type='text' size=8 name='clean'/></td>
+	<td><input type='text' size=8 name='fund'/></td>
+	<td>&nbsp;</td>
+	<td><input type='text' size=30 name='explan'/></td>
+	</form></tr></table></td></tr>");
+
 $total=mysql_fetch_array($total_sql, MYSQL_ASSOC);
 printf("<tr align='right'>
 	<th>&nbsp;</th>
@@ -202,6 +246,8 @@ printf("<tr align='right'>
 	<th>&nbsp;</th>
 	</tr>\n");
 printf("</table>\n");
+printf("<input type='button' onclick='add_row()' value='Добавяне'/>\n");
+printf("<input type='button' onclick='remove_row()' value='Premahvane'/>\n");
 
 if (!mysql_close($connect)) {
 	die("There is a problem with closing the connection<br/>");
