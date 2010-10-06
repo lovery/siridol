38bf892692551971981b01d5f5200061743a0562
root <root@hag.hw.v5d.org>
2010-10-06 22:53:07 +0300

    Use logical "and" instead of bitwise "and"

--- index.php
+++ index.php
@@ -177,7 +177,7 @@ if (!$sql_result) {
 
 $is_date='ne e data';
 while ($array=mysql_fetch_array($sql_result, MYSQL_ASSOC)) {
-	if ($is_date!=$array[month] & $_GET["sort"]=="1" & $is_date!='ne e data') {
+	if ($is_date!=$array[month] && $_GET["sort"]=="1" && $is_date!='ne e data') {
 		printf("<tr>\n");
 		printf("<td class='nonegative' colspan=2>Общо за месец ".$is_date."</td>\n");
 		print_html_num_td($is_rub);
