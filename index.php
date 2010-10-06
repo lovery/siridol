bd220c169aea96223e0a74fa95d42ade7de194ec
root <root@hag.hw.v5d.org>
2010-10-06 19:53:48 +0300

    Rename boqna-accountancy.php to index.php
    So the URL can be http://siridol.v5d.org/ instead
    of http://siridol.v5d.org/boqna-accountancy.php
    
    Also rename the CSS file.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<link rel="stylesheet" type="text/css" href="boqna-accountancy.css">
+<link rel="stylesheet" type="text/css" href="index.css">
 <title>
 Счетоводство на комплекса
 </title>
