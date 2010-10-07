6380f0edf68de3f6ebbbdd05893da9f43d2ed6a5
root <root@hag.hw.v5d.org>
2010-10-07 14:19:04 +0300

    Add id column for easier manipulation in SQL

--- create.sql
+++ create.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS `accountancy`;
 CREATE TABLE `accountancy` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`month` DATE NOT NULL,
 	`on_date` DATE NOT NULL,
 	`rubbish` FLOAT(10,2) NOT NULL,
@@ -7,5 +8,6 @@ CREATE TABLE `accountancy` (
 	`homemanager` FLOAT(10,2) NOT NULL,
 	`cleanstreets` FLOAT(10,2) NOT NULL,
 	`fund` FLOAT(10,2) NOT NULL,
-	`explanation` VARCHAR(150) NOT NULL
+	`explanation` VARCHAR(150) NOT NULL,
+	PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8;
